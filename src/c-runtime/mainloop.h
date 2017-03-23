4dc3e195d40a85ab9ac6e3ab75dfa93d610f008b
Collin RM Stocks <collinstocks@gmail.com>
2017-03-22 20:08:00 -0400

    - push a thread, not a function - C functions called from Lua start with 20 free stack slots, so no need to try to extend the stack

---
+++ src/c-runtime/mainloop.h
@@ -0,0 +1,13 @@
+#ifndef CR_MAINLOOP_H
+#define CR_MAINLOOP_H
+
+#include "lj_headers.h"
+
+
+/*
+  loop_once(...tbd...)
+*/
+int cr_loop_once(lua_State *);
+
+
+#endif
--- src/c-runtime/resumer.c
+++ src/c-runtime/resumer.c
@@ -11,9 +11,6 @@
 */
 static int cr_resume(lua_State *L) {
   int nargs = lua_gettop(L);              // stack: [args...]
-  if (!lua_checkstack(L, 2)) {
-    return luaL_error(L, "cr_resume() could not grow stack");
-  }
   lua_pushvalue(L, lua_upvalueindex(1));  // stack: [args..., resume_to]
   if (lua_pushthread(L)) {                // stack: [args..., resume_to, current coroutine]
     return luaL_error(L, "cr_resume() called from the main thread");
@@ -30,6 +27,10 @@ int cr_new_Resumer(lua_State *L) {
   } else if (lua_iscfunction(L, 1) || !lua_isfunction(L, 1)) {
     return luaL_error(L, "cr_new_Resumer() expected lua function");
   }
-  lua_pushcclosure(L, &cr_resume, 1);
+  lua_State *thread = lua_newthread(L);   // stack: [function, thread]
+  lua_insert(L, -2);                      // stack: [thread, function]
+  lua_xmove(L, thread, 1);                // stack: [thread]
+  // thread stack: [function]
+  lua_pushcclosure(L, &cr_resume, 1);     // stack: [resumer(thread[function])]
   return 1;
 }
